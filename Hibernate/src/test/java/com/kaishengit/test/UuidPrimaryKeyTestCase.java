01517e5fa78bbb9f17b57b62f849b4caaca446fb
anze-21 <zml_anze_92@163.com>
2016-07-27 17:19:59 +0800

    演示Hibernate一对一、多对多和缓存机制

--- Hibernate/src/test/java/com/kaishengit/test/UuidPrimaryKeyTestCase.java
+++ Hibernate/src/test/java/com/kaishengit/test/UuidPrimaryKeyTestCase.java
@@ -29,25 +29,23 @@ public class UuidPrimaryKeyTestCase {
         //一级缓存
         Task task = (Task) session.get(Task.class,"8a8082e4562a08fa01562a0901df0000");
         System.out.println(session.contains(task));
-        Task task2 = (Task) session.get(Task.class,"8a8082e4562a08fa01562a0901df0000");
-        System.out.println(session.contains(task2));
         //session.clear();//持久态 -> 游离态  并清空一级缓存
        // session.evict(task);//将指定对象从一级缓存中清除
         session.getTransaction().commit();
         //------------------------------------------------
 
-//        //将对象从二级缓存中清除
-//               Cache cache =HibernateUtil.getSessionFactory().getCache();
-//        cache.evictEntityRegion(Task.class);
-//        cache.evictAllRegions();
-//
-//        Session session2 = HibernateUtil.getSession();
-//        session2.beginTransaction();
-//
-//        Task task2 = (Task) session2.get(Task.class,"8a8082e454bc3f1b0154bc3f1f6a0000");
-//        System.out.println(task2.getTitle());
-//
-//        session2.getTransaction().commit();
+        //将对象从二级缓存中清除
+               Cache cache =HibernateUtil.getSessionFactory().getCache();
+        cache.evictEntityRegion(Task.class);
+        cache.evictAllRegions();
+
+        Session session2 = HibernateUtil.getSession();
+        session2.beginTransaction();
+
+        Task task2 = (Task) session2.get(Task.class,"8a8082e454bc3f1b0154bc3f1f6a0000");
+        System.out.println(task2.getTitle());
+
+        session2.getTransaction().commit();
 
     }
 }
